83c89ce0e3de76b22bcd7172ebc3e43f6f6f2041
rajat <rajat.ambedkar179@gmail.com>
2025-04-09 13:10:57 +0530

    added next in the keyword

--- src/utils/metaData.tsx
+++ src/utils/metaData.tsx
@@ -1,7 +1,7 @@
 export const METADATA = {
     title: "User Listing | Browse and Connect with Top Users",
     description: "Explore a comprehensive list of users. Browse through profiles, discover new connections, and find the right individuals for your needs. Join our community today!",
-    keywords: "user listing, user directory, find users, browse profiles, top users, user community, connect with users",
+    keywords: "next user listing, user directory, find users, browse profiles, top users, user community, connect with users",
     robots:"index, follow",
     creator: "Kamal"
 }
